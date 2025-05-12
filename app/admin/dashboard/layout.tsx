dbe85d57bddd8d112b297d50c537238608a9792f
Danya-web <danilameretov20@gmail.com>
2025-05-12 17:24:26 +0500

    added: Uploadthing

--- app/admin/dashboard/layout.tsx
+++ app/admin/dashboard/layout.tsx
@@ -1,8 +1,17 @@
-export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+import { ourFileRouter } from "@/app/api/uploadthing/core";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
 
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
     return (
         <div className="p-4">
+            <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
             {children}
         </div>
-    )
+    );
 }
+
--- app/admin/dashboard/new/page.tsx
+++ app/admin/dashboard/new/page.tsx
@@ -1,37 +1,45 @@
-import { redirect } from "next/navigation";
-import { addProduct } from "../actions";
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 import Link from "next/link";
 import { FaArrowLeftLong } from "react-icons/fa6";
+import { UploadButton } from "@/src/utils/uploadthing";
+import { MdCloudUpload } from "react-icons/md";
+import { addProduct } from "../actions";
 
 export default function NewProductPage() {
-    async function createProduct(formData: FormData) {
-        "use server";
+    const [imageUrl, setImageUrl] = useState(""); 
+    const router = useRouter(); 
 
+    const createProduct = async (event: React.FormEvent) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target as HTMLFormElement);
         const title = formData.get("title") as string;
         const description = formData.get("description") as string;
         const price = parseFloat(formData.get("price") as string);
         const imageUrl = formData.get("imageUrl") as string;
 
-        if (isNaN(price) || !title || !description || !imageUrl) {
-            throw new Error("Missing required fields");
+        try {
+            await addProduct(title, description, price, imageUrl);
+            router.push("/admin/dashboard"); 
+        } catch (error) {
+            console.error(error);
+            alert("Ошибка при создании товара");
         }
-
-        await addProduct(title, description, price, imageUrl);
-        redirect("/admin/dashboard");
-    }
+    };
 
     return (
         <div className="max-w-xl mx-auto p-4">
             <div>
-                <Link
-                    href="/admin/dashboard"
-                    className="flex items-center gap-2 text-black"
-                >
-                    <FaArrowLeftLong />Назад
+                <Link href="/admin/dashboard" className="flex items-center gap-2 text-black">
+                    <FaArrowLeftLong /> Назад
                 </Link>
             </div>
             <h1 className="text-2xl font-bold mb-6">Добавить товар</h1>
-            <form action={createProduct} className="space-y-4">
+            <form onSubmit={createProduct} className="space-y-4">
                 <div>
                     <label htmlFor="title" className="block mb-1 font-semibold">
                         Название
@@ -69,18 +77,41 @@ export default function NewProductPage() {
                         className="w-full border p-2 rounded"
                     />
                 </div>
-                <div>
-                    <label htmlFor="imageUrl" className="block mb-1 font-semibold">
-                        Ссылка на изображение
+
+                <div className="space-y-2">
+                    <label className="block text-base font-medium text-gray-700">
+                        Картинка
                     </label>
-                    <input
-                        type="url"
-                        id="imageUrl"
-                        name="imageUrl"
-                        required
-                        className="w-full border p-2 rounded"
-                    />
+
+                    <div className="border-2 border-dashed border-gray-300 rounded-lg p-6 flex flex-col items-center justify-center text-center space-y-3">
+                        <div className="text-gray-400 text-4xl">
+                            <MdCloudUpload className="text-gray-400 cursor-pointer" />
+                        </div>
+
+                        <p className="text-sm text-gray-600">Выберите файл</p>
+
+                        <label className="cursor-pointer bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-200">
+                            Upload File
+                            <UploadButton
+                                endpoint="imageUploader"
+                                onClientUploadComplete={(res: any) => {
+                                    const url = res?.[0]?.url;
+                                    if (url) {
+                                        setImageUrl(url); // Сохраняем URL изображения
+                                        alert("Загрузка завершена");
+                                    }
+                                }}
+                                onUploadError={(error: Error) => {
+                                    alert(`Ошибка загрузки: ${error.message}`);
+                                }}
+                                className="hidden"
+                            />
+                        </label>
+                    </div>
+
+                    {imageUrl && <input type="hidden" name="imageUrl" value={imageUrl} />}
                 </div>
+
                 <button
                     type="submit"
                     className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
@@ -91,4 +122,3 @@ export default function NewProductPage() {
         </div>
     );
 }
-
